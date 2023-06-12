1f5e66226a5633d931c334d11be0217f49c9602c
a-voinov <alariht@gmail.com>
2023-06-12 20:42:42 +0700

    reformat modules to separate files

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,4 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-
-        fn seat_at_table() {}
-    }
-
-    mod serving {
-        fn take_order() {}
-
-        fn serve_order() {}
-
-        fn take_payment() {}
-    }
-}
+mod front_of_house;
 
 use crate::front_of_house::hosting;
 
@@ -25,15 +11,4 @@ pub fn eat_at_restaurant() {
 
     // Shortcut path
     hosting::add_to_waitlist();
-}
-
-fn deliver_order() {}
-
-mod back_of_house {
-    fn fix_incorrect_order() {
-        cook_order();
-        super::deliver_order();
-    }
-
-    fn cook_order() {}
 }
